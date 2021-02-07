bad34b29a8c1635eba548a1a5f22438bb3922944
samschter <samschter@users.noreply.github.com>
2021-02-07 01:39:46 +0100

    Update LANs.py

--- LANs.py
+++ LANs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Description:   ARP poisons a LAN victim and prints all the interesting unencrypted info like usernames, passwords and messages. Asynchronous multithreaded arp spoofing packet parser.
